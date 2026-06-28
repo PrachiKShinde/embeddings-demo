38bac356b448a4464444df98e13ca52a8b44c40b
Prachi Shinde <prachishinde31801@gmail.com>
2026-06-28 19:14:43 +0530

    Update embeddings_demo.py

--- embeddings_demo.py
+++ embeddings_demo.py
@@ -2,22 +2,20 @@ from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-documents = [
-    "SwiftUI is Apple's framework for building user interfaces on iOS.",
-    "Kotlin is commonly used for Android development.",
-    "Dogs are friendly animals.",
-    "Vector databases store embeddings for similarity search."
-]
+with open("doc_intro.txt", "r") as file:
+    document = file.read()
 
-query = "I dont like pet animals at all"
+documents = document.split("\n\n")
+
+query = "Where can I view DocC documentation?"
 
 doc_embeddings = model.encode(documents)
+
 query_embedding = model.encode(query)
 
 scores = model.similarity(query_embedding, doc_embeddings)
 
-print(scores)
-
 best_match_index = scores.argmax()
-print("Best matching document:")
+
+print("Best matching paragraph:\n")
 print(documents[best_match_index])
